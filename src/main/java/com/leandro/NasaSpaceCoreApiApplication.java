6739b29267c15720e87f0f220438f1f27199208c
leandro.brito <leandro.brito@memora.com.br>
2025-11-14 09:12:03 -0300

    Projeto inicial NasaSpaceCore

---
+++ src/main/java/com/leandro/NasaSpaceCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.leandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaSpaceCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaSpaceCoreApiApplication.class, args);
+	}
+
+}
